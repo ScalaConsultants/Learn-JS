4cf077492fe3f663dfcdb98157d6dec8be0f9929
Tomasz Sosinski <tomasz.sosinski@outlook.com>
2016-08-30 15:45:08 +0200

    Improvements to styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,21 +4,23 @@ import RecipesList from '../components/RecipesList';
 let recipes = [{name: 'Foo', description: 'Foooooooo', image: require('../images/wings.jpg')},
               {name: 'Bar', description: 'Baaar', image: require('../images/tortilla.jpg')},
               {name: 'Baz', description: 'Baz', image: require('../images/cheeserolls.jpg')},
-              {name: 'Foo2', description: 'Foooooooo', image: require('../images/tortilla.jpg')},
-              {name: 'Bar2', description: 'Baaar', image: require('../images/wings.jpg')},
-              {name: 'Baz2', description: 'Baz', image: require('../images/tortilla.jpg')},
-              {name: 'Foo3', description: 'Foooooooo', image: require('../images/cheeserolls.jpg')},
+              {name: 'Foo2', description: 'Foooooooo', image: require('../images/wings.jpg')},
+              {name: 'Bar2', description: 'Baaar', image: require('../images/tortilla.jpg')},
+              {name: 'Baz2', description: 'Baz', image: require('../images/cheeserolls.jpg')},
+              {name: 'Foo3', description: 'Foooooooo', image: require('../images/wings.jpg')},
               {name: 'Bar3', description: 'Baaar', image: require('../images/tortilla.jpg')},
-              {name: 'Baz3', description: 'Baz', image: require('../images/wings.jpg')},
-              {name: 'Foo4', description: 'Foooooooo', image: require('../images/tortilla.jpg')},
-              {name: 'Bar4', description: 'Baaar', image: require('../images/cheeserolls.jpg')},
-              {name: 'Baz4', description: 'Baz', image: require('../images/wings.jpg')}];
+              {name: 'Baz3', description: 'Baz', image: require('../images/cheeserolls.jpg')},
+              {name: 'Foo4', description: 'Foooooooo', image: require('../images/wings.jpg')},
+              {name: 'Bar4', description: 'Baaar', image: require('../images/tortilla.jpg')},
+              {name: 'Baz4', description: 'Baz', image: require('../images/cheeserolls.jpg')}];
 
 class App extends React.Component {
 	render() {
 		return(
 			<div>
-				<p>Premium leader</p>
+                            <div id="mainImg">
+				   <img src={require('../images/banner.jpg')} />
+                            </div>
 				<RecipesList data={recipes} />
 			</div>
 			)
