327f4589485ae718e3d95f0e95605d5de7c9b5b3
Tomasz Sosinski <tomasz.sosinski@outlook.com>
2016-09-07 15:03:02 +0200

    Should introduce propTypes and formatting changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import RecipesList from '../components/RecipesList';
 
-let recipes = [{name: 'Foo', description: 'Foooooooo', image: require('../images/wings.jpg')},
-              {name: 'Bar', description: 'Baaar', image: require('../images/tortilla.jpg')},
-              {name: 'Baz', description: 'Baz', image: require('../images/cheeserolls.jpg')},
-              {name: 'Foo2', description: 'Foooooooo', image: require('../images/wings.jpg')},
-              {name: 'Bar2', description: 'Baaar', image: require('../images/tortilla.jpg')},
-              {name: 'Baz2', description: 'Baz', image: require('../images/cheeserolls.jpg')},
-              {name: 'Foo3', description: 'Foooooooo', image: require('../images/wings.jpg')},
-              {name: 'Bar3', description: 'Baaar', image: require('../images/tortilla.jpg')},
-              {name: 'Baz3', description: 'Baz', image: require('../images/cheeserolls.jpg')},
-              {name: 'Foo4', description: 'Foooooooo', image: require('../images/wings.jpg')},
-              {name: 'Bar4', description: 'Baaar', image: require('../images/tortilla.jpg')},
-              {name: 'Baz4', description: 'Baz', image: require('../images/cheeserolls.jpg')}];
+let recipes = [{ name: 'Foo', description: 'Foooooooo', image: require('../images/wings.jpg') },
+    { name: 'Bar', description: 'Baaar', image: require('../images/tortilla.jpg') },
+    { name: 'Baz', description: 'Baz', image: require('../images/cheeserolls.jpg') },
+    { name: 'Foo2', description: 'Foooooooo', image: require('../images/wings.jpg') },
+    { name: 'Bar2', description: 'Baaar', image: require('../images/tortilla.jpg') },
+    { name: 'Baz2', description: 'Baz', image: require('../images/cheeserolls.jpg') },
+    { name: 'Foo3', description: 'Foooooooo', image: require('../images/wings.jpg') },
+    { name: 'Bar3', description: 'Baaar', image: require('../images/tortilla.jpg') },
+    { name: 'Baz3', description: 'Baz', image: require('../images/cheeserolls.jpg') },
+    { name: 'Foo4', description: 'Foooooooo', image: require('../images/wings.jpg') },
+    { name: 'Bar4', description: 'Baaar', image: require('../images/tortilla.jpg') },
+    { name: 'Baz4', description: 'Baz', image: require('../images/cheeserolls.jpg') }
+];
 
 class App extends React.Component {
-	render() {
-		return(
-			<div>
-                            <div id="mainImg">
-				   <img src={require('../images/banner.jpg')} />
-                            </div>
-				<RecipesList data={recipes} />
-			</div>
-			)
-	}
+    render() {
+        return (
+            <div>
+                     <div id = "mainImg">
+                            <img src = { require('../images/banner.jpg') }/>
+                     </div>
+                     <RecipesList data = { recipes }/>
+              </div>
+        )
+    }
 }
 
 export default App;
