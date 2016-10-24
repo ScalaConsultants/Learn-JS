d20867d54c4180eb8de2d617c0c2ec0d1a8dbd36
Tomasz Sosinski <tomasz.sosinski@scalac.io>
2016-10-24 17:01:43 +0200

    Removed commented functions

--- server/index.js
+++ server/index.js
@@ -65,31 +65,4 @@ app.use('/api', router);
 
 app.listen(port);
 console.log('The server is running on http://localhost:' + port);
-//
-// function saveDataToFile(data) {
-//   "use strict";
-//   fs.writeFile('./server/models/recipes.json',
-//     JSON.stringify(data),
-//     function (err) {
-//       "use strict";
-//       if (err) {
-//         console.error(err)
-//       }
-//
-//       console.log("Data successfully saved to a file.")
-//     });
-// }
-//
-// function initDataLoad(response) {
-//   fs.readFile('./server/models/recipes.json', function (err, data) {
-//     if (err) {
-//       response.json(err)
-//     } else {
-//       cache['data'] = JSON.parse(data);
-//       response.json({
-//         info: 'Retrieved from file',
-//         data: JSON.parse(data)
-//       })
-//     }
-//   })
-// }
+
